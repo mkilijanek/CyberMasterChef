19367994922c51fb94db8958aa32143c5eb348d8
mkilijanek <mkilijanek@users.noreply.github.com>
2026-02-24 19:55:14 +0100

    Fix regex in text.removeDollarSigns

--- packages/plugins-standard/src/ops/removeDollarSigns.ts
+++ packages/plugins-standard/src/ops/removeDollarSigns.ts
@@ -9,6 +9,6 @@ export const removeDollarSigns: Operation = {
   args: [],
   run: ({ input }) => {
     if (input.type !== "string") throw new Error("Expected string input");
-    return { type: "string", value: input.value.replace(/$+/gu, "") };
+    return { type: "string", value: input.value.replace(/\$+/gu, "") };
   }
 };
